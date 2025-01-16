d3cd26091192b7efbaf09f32f399e93becbcd044
Finn <lehuynhtuong13@gmail.com>
2025-01-16 15:12:40 +0700

    Update Navbar.jsx

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -4,10 +4,12 @@ import {
   SearchOutlined,
   ShoppingCartOutlined,
   UserOutlined,
+  MenuOutlined,
+  LoginOutlined,
+  UserAddOutlined,
 } from "@ant-design/icons";
 import { Dropdown, Menu } from "antd";
 import { assets } from "../web/assets/assets";
-import AllProducts from "../web/components/homepage/AllProduct";
 import Header from "./Header"
 const Navbar = () => {
   const [isMobileMenuVisible, setIsMobileMenuVisible] = useState(false);
@@ -33,17 +35,16 @@ const Navbar = () => {
   }, [location.pathname]);
 
   const userMenu = (
-    <Menu style={{ width: "120px", fontSize: "16px" }}>
-      {
-        <>
-          <Menu.Item key="1">
-            <Link to="/login"> Đăng nhập</Link>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Link to="/register"> Đăng ký</Link>
-          </Menu.Item>
-        </>
-      }
+    <Menu style={{ width: "120px", fontSize: "10px" }}>
+      {/* Đăng nhập */}
+      <Menu.Item key="1" icon={<LoginOutlined style={{ fontSize: "16px" }} />} >
+        <Link to="/login">Đăng nhập</Link>
+      </Menu.Item>
+
+      {/* Đăng ký */}
+      <Menu.Item key="2" icon={<UserAddOutlined style={{ fontSize: "16px" }} />} >
+        <Link to="/register">Đăng ký</Link>
+      </Menu.Item>
     </Menu>
   );
 
@@ -51,146 +52,51 @@ const Navbar = () => {
     <>
       <Header />
       <div className="flex items-center justify-between py-5 font-medium sm:mt-32 xs:mt-32 xxs:mt-32 lg:mt-10 mt-16">
-        <Link to="/">
-          <img src={assets.logo} className="w-36" alt="Logo" />
-        </Link>
-        <div className="flex items-center gap-12 ml-auto">
-          <ul className="hidden md:flex gap-24 text-md font-semibold text-gray-800">
-            <NavLink
-              to="/"
-              className="nav-link flex flex-col items-center gap-3"
-              onClick={() => setSelectedItem("home")}
-            >
-              <p className="text">TRANG CHỦ</p>
-              <hr
-                className={`w-2/4 border-none h-[1.5px] bg-gray-700 ${selectedItem === "home" ? "" : "hidden"
-                  }`}
-              />
-            </NavLink>
-            <NavLink
-              to="/postcategory/all"
-              className="nav-link flex flex-col items-center gap-3"
-              onClick={() => setSelectedItem("news")}
-            >
-              <p>TIN TỨC</p>
-              <hr
-                className={`w-2/4 border-none h-[1.5px] bg-gray-700 ${selectedItem === "news" ? "" : "hidden"
-                  }`}
-              />
-            </NavLink>
-            <NavLink
-              to="/about"
-              className="nav-link flex flex-col items-center gap-3"
-              onClick={() => setSelectedItem("about")}
-            >
-              <p>VỀ CHÚNG TÔI</p>
-              <hr
-                className={`w-2/4 border-none h-[1.5px] bg-gray-700 ${selectedItem === "about" ? "" : "hidden"
-                  }`}
-              />
-            </NavLink>
-            <NavLink
-              to="/contact"
-              className="nav-link flex flex-col items-center gap-3"
-              onClick={() => setSelectedItem("contact")}
-            >
-              <p>LIÊN HỆ</p>
-              <hr
-                className={`w-2/4 border-none h-[1.5px] bg-gray-700 ${selectedItem === "contact" ? "" : "hidden"
-                  }`}
-              />
-            </NavLink>
-          </ul>
-        </div>
-        <div className="flex items-center gap-12 ml-auto">
-          <SearchOutlined
-            style={{ fontSize: "35px" }}
-            onClick={() => setShowSearch((prev) => !prev)}
-            className="cursor-pointer hover:scale-110 hover:text-blue-500 transition-transform duration-300 ease-in-out"
-            alt="Search Icon"
-          />
-          <div className="relative">
-            <Link to="/cart">
-              <ShoppingCartOutlined
-                style={{ fontSize: "35px" }}
-                className="cursor-pointer hover:scale-110 hover:text-green-500 transition-transform duration-300 ease-in-out"
-                alt="Cart Icon"
-              />
-              <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
-                {/* Insert cart count */}
-              </p>
-            </Link>
+        {/* Logo và icon Danh mục */}
+        <div className="flex items-center gap-6">
+          <Link to="/">
+            <img src={assets.logo} className="w-32" alt="Logo" />
+          </Link>
+          <div className="flex items-center gap-2 rounded-full bg-red-500 w-32">
+            <button className="flex items-center justify-center w-10 h-10 text-white hover:bg-red-600 transition">
+              <MenuOutlined style={{ fontSize: "20px" }} />
+            </button>
+            <span className="text-white text-base leading-relaxed font-bold">Danh mục</span>
           </div>
-          <Dropdown menu={{
-            items: [
-              { label: <Link to="/login">Đăng nhập</Link>, key: '1' },
-              { label: <Link to="/register">Đăng ký</Link>, key: '2' }
-            ]
-          }} trigger={['click']}>
+        </div>
+        {/* Thanh tìm kiếm */}
+        <div className="flex-1 mx-6 border-gray-200 border-2 rounded-full">
+          <div className="relative">
+            <input
+              type="text"
+              className="w-full py-2 px-4 border-gray-100 rounded-full text-gray-700 focus:outline-none"
+              placeholder="Nhập tên điện thoại, máy tính, phụ kiện... cần tìm"
+            />
+            <button className="absolute top-1/2 transform -translate-y-1/2 right-4 text-gray-700 hover:text-gray-900">
+              <SearchOutlined style={{ fontSize: "25px" }} />
+            </button>
+          </div>
+        </div>
+
+        {/* Icon người dùng và giỏ hàng */}
+        <div className="flex items-center gap-6">
+          <Dropdown overlay={userMenu} trigger={["click"]}>
             <UserOutlined
-              style={{ fontSize: '35px' }}
-              className='cursor-pointer hover:scale-110 hover:text-purple-500 transition-transform duration-300 ease-in-out'
+              style={{ fontSize: "30px" }}
+              className="cursor-pointer hover:scale-110 transition-transform"
             />
           </Dropdown>
-
-          <img
-            onClick={() => setIsMobileMenuVisible(true)}
-            src={assets.menu_icon}
-            className="w-5 cursor-pointer md:hidden"
-            alt="Menu Icon"
-          />
-        </div>
-        <div
-          className={`absolute top-0 right-0 bottom-0 mt-32 overflow-hidden bg-white transition-all ${isMobileMenuVisible ? "w-full z-50" : "w-0"
-            }`}
-        >
-          <div className="flex flex-col text-gray-600">
-            <div
-              onClick={() => setIsMobileMenuVisible(false)}
-              className="flex items-center gap-4 p-3 cursor-pointer"
-            >
-              <img
-                src={assets.dropdown_icon}
-                className="h-4 rotate-180"
-                alt="Dropdown Icon"
+          <Link to="/cart">
+            <div className="relative">
+              <ShoppingCartOutlined
+                style={{ fontSize: "30px" }}
+                className="cursor-pointer hover:scale-110 transition-transform"
               />
-              <p>Back</p>
+              <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs w-4 h-4 rounded-full flex items-center justify-center">
+                3
+              </span>
             </div>
-            <NavLink
-              onClick={() => setIsMobileMenuVisible(false)}
-              className="py-2 pl-6 border menu-item"
-              to="/"
-            >
-              TRANG CHỦ
-            </NavLink>
-            <div className="py-2 pl-6 border menu-item">
-              <AllProducts
-                closeMenu={() => setIsMobileMenuVisible(false)}
-                onSelectCollection={() => setSelectedItem("collection")}
-              />
-            </div>
-            <NavLink
-              onClick={() => setIsMobileMenuVisible(false)}
-              className="py-2 pl-6 border menu-item"
-              to="/postcategory/all"
-            >
-              TIN TỨC
-            </NavLink>
-            <NavLink
-              onClick={() => setIsMobileMenuVisible(false)}
-              className="py-2 pl-6 border menu-item"
-              to="/about"
-            >
-              VỀ Chúng tôi
-            </NavLink>
-            <NavLink
-              onClick={() => setIsMobileMenuVisible(false)}
-              className="py-2 pl-6 border menu-item"
-              to="/contact"
-            >
-              LIÊN HỆ
-            </NavLink>
-          </div>
+          </Link>
         </div>
       </div>
     </>
